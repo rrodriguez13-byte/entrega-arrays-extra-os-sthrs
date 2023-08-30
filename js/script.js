ac849247ec00389cef174af08138ec18f9f5d0b5
rodriguez13-byte <leguizamon3718@gmail.com>
2023-08-30 10:38:10 -0300

    solucion

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  
+  // Filtrar elementos de tipo String
+  const stringElements = array.filter(element => typeof element === "string");
+  
+  // Ordenar elementos alfabéticamente
+    const sortedElements = stringElements.sort();
+  
+  // Mostrar elementos en la página
+  sortedElements.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  showList(strangeArray);
+});
